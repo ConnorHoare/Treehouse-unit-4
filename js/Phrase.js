c946c0e68e4e3e35381725f4eb52ac0a0bc321f8
ConnorHoare <connor@tipcigroup.com>
2022-10-18 18:22:26 +0100

    update phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,6 +7,8 @@
      this.phrase = phrase.toLowerCase();
    }
 
+   let phraseCharArr = [];
+
    addPhraseToDisplay() {
     //  Adds letter placeholders to the display when the game starts
     //  If we take length of the phrase passed into constructor above and loop through to create an LI for every letter
@@ -25,16 +27,40 @@
         li[i].classList.add("hide");
         li[i].classList.add("letter");
         li[i].classList.add(`${li[i].innerText}`);
+        phraseCharArr.push(li[i].innerText);
       }
       phraseUL.append(li);
     }
    }
 
-   checkLetter() {
+   let matchedLetterArr = [];
+
+   checkLetter(e) {
      // Checks to see if the letter selected by the player matches a letter in the phrase
+     // Get the current selected buttons inner text
+     // Use if statement and includes method to check if the current buttons inner text matches the phrase.
+     // const buttons = document.getElementById('keyrow').getElementsByTagName("BUTTON");
+
+     if (phrase.includes(e.target.value)) {
+       matchedLetterArr.push(e.target.value)
+     } else {
+       console.log("Incorrect");
+     }
+
    }
 
    showMatchedLetter() {
      // Reveals the letters on the board that the player has matched.
+
+     // compare the phrase to the clicked buttons
+     // if the buttons letter in included in the phrase -> change the class property of all li's who's innerText matches the buttons
+
+     const li = phraseUL.querySelectorAll('letter');
+     for ( var i = 0; i < li.length; i++) {
+       if(matchedLetterArr.includes(li[i].innerText)) {
+         li[i].classList.remove("hide");
+         li[i].classList.add("show");
+       }
+     }
    }
  }
